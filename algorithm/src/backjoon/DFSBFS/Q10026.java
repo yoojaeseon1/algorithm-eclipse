aa3e4bcbed1963f34ded0861786635de03c40cb7
jaeseonyoo <yoojs3695@gmail.com>
2019-04-22 16:51:29 +0900

    10026(적록색약)-완성

--- algorithm/src/backjoon/DFSBFS/Q10026.java
+++ algorithm/src/backjoon/DFSBFS/Q10026.java
@@ -1,4 +1,4 @@
-package backjoon;
+package backjoon.DFSBFS;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Main {
+public class Q10026 {
 
 	public static void main(String[] args) throws IOException {
 
@@ -107,6 +107,7 @@ public class Main {
 				}
 			}
 		}
+
 		System.out.println(rgbCount + " " + rgCount);
 	}
 }
