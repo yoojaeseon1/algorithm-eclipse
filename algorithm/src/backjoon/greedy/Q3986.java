8ba6b71bcfa249681a7c9c35e1572f471cd1d7d2
DESKTOP-ISLTEUA\yoo-pc <yoojs3695@gmail.com>
2019-06-05 01:35:35 +0900

    완성 - 좋은단어(3986)

--- algorithm/src/backjoon/greedy/Q3986.java
+++ algorithm/src/backjoon/greedy/Q3986.java
@@ -1,11 +1,11 @@
-package backjoon;
+package backjoon.greedy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main {
-
+public class Q3986 {
+	
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,4 +38,5 @@ public class Main {
 		}
 		System.out.println(goodWordCount);
 	}
+
 }
