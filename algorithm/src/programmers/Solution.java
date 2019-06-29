de57c1581cc118b590fdd191f9c549b9564c795f
DESKTOP-ISLTEUA\yoo-pc <yoojs3695@gmail.com>
2019-06-30 02:59:03 +0900

    완성 - Solution. 추가(package) - programmers

---
+++ algorithm/src/programmers/Solution.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		int[][] results = {{4,3},{4,2},{3,2},{1,2},{2,5}};
+	
+		
+		
+		
+
+	}
+}
