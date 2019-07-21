a9a14df5e55d73d7fb4dd76a5e1464e735ba2253
jaeseonyoo <yoojs3695@gmail.com>
2019-07-21 15:42:29 +0900

    완성 - Q1149(RGB거리)

--- algorithm/src/backjoon/DP/Q1149.java
+++ algorithm/src/backjoon/DP/Q1149.java
@@ -1,11 +1,11 @@
-package backjoon;
+package backjoon.DP;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class Q1149 {
+	
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -60,4 +60,5 @@ public class Main {
 
 		System.out.println(minCostSum);
 	}
+
 }
