7bfc5ae3764ca928b61e79a499ea747f596df8e8
jaeseonyoo <yoojs3695@gmail.com>
2019-07-26 21:08:17 +0900

    완성 - 스티커(9465)

--- algorithm/src/backjoon/DP/Q9465.java
+++ algorithm/src/backjoon/DP/Q9465.java
@@ -1,11 +1,11 @@
-package backjoon;
+package backjoon.DP;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class Q9465 {
+	
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -47,4 +47,5 @@ public class Main {
 			
 		}
 	}
+
 }
