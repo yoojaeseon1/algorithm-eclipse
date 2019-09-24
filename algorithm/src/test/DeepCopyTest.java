7b0c7f0e69d72f4bdc1aab16e13ec153ad242f3b
DESKTOP-ISLTEUA\yoo-pc <yoojs3695@gmail.com>
2019-09-25 00:47:38 +0900

    수정 - keyPoint(Algorithm) : visited 배열을 deepCopy할 필요가 없는 이유

--- algorithm/src/test/DeepCopyTest.java
+++ algorithm/src/test/DeepCopyTest.java
@@ -7,67 +7,74 @@ public class DeepCopyTest {
 
 	public static void main(String[] args) {
 
-//		int[] source = { 1, 2, 3, 4 };
-//		int[] dest = source;
-		
+		// int[] source = { 1, 2, 3, 4 };
+		// int[] dest = source;
 
-//		System.out.println(dest[1]);
-//
-//		source[1] = 5;
-//
-//		System.out.println(dest[1]);
+		// System.out.println(dest[1]);
+		//
+		// source[1] = 5;
+		//
+		// System.out.println(dest[1]);
 
-//		System.out.println(source[1]);
-//
-//		dest[1] = 5;
-//
-//		System.out.println(source[1]);
-		
-//		boolean[] source = new boolean[26];
-//		
-//		DeepCopy test = new DeepCopy(1, source);
-//		
-//		System.out.println(test.getVisited()[5]);
-//		
-//		source[5] = true;
-//		
-//		System.out.println(test.getVisited()[5]);
-//
-		
-//		List<Integer> sourceList = new ArrayList<>();
-//		
-//		sourceList.add(1);
-//		sourceList.add(2);
-//		sourceList.add(3);
-//		sourceList.add(4);
-		
-//		List<Integer> destList =List 
-		
-		
-		int[] source = {1,2,3,4};
-		int[] dest = {1,2,3,4};
-		
+		// System.out.println(source[1]);
+		//
+		// dest[1] = 5;
+		//
+		// System.out.println(source[1]);
+
+		// boolean[] source = new boolean[26];
+		//
+		// DeepCopy test = new DeepCopy(1, source);
+		//
+		// System.out.println(test.getVisited()[5]);
+		//
+		// source[5] = true;
+		//
+		// System.out.println(test.getVisited()[5]);
+		//
+
+		// List<Integer> sourceList = new ArrayList<>();
+		//
+		// sourceList.add(1);
+		// sourceList.add(2);
+		// sourceList.add(3);
+		// sourceList.add(4);
+
+		// List<Integer> destList =List
+
+		int[] source = { 6, 7, 8, 9, 0 ,11, 22};
+		int[] dest = { 1, 2, 3, 4 };
+
 		dest[0] = 5;
-		
-		System.out.println(source[0]);
-		System.out.println(dest[0]);
+
+		// System.out.println(source[0]);
+		// System.out.println(dest[0]);
+
+		copyDeeply(source, 2);
+
+		System.out.println(source[1]);
 
 	}
 
+	public static void copyDeeply(int[] source, int executingCount) {
+		source[executingCount] = executingCount;
+		if (executingCount <= 5)
+			copyDeeply(source, executingCount + 1);
+	}
 }
 
-class DeepCopy{
-	
+class DeepCopy {
+
 	private int x;
 	private boolean[] visited = new boolean[26];
-	
+
 	public DeepCopy() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public DeepCopy(int x, boolean[] visited) {
 		this.x = x;
-//		this.visited = visited;
+		// this.visited = visited;
 		System.arraycopy(visited, 0, this.visited, 0, visited.length);
 	}
 
@@ -86,7 +93,5 @@ class DeepCopy{
 	public void setVisited(boolean[] visited) {
 		this.visited = visited;
 	}
-	
-	
-	
+
 }
